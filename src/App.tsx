31dc9d6e125e0f93422467c40e4b3cca14609e26
himart-gj <aa01051745672@gmail.com>
2026-09-14 15:26:05 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -240,7 +240,8 @@ export default function App() {
                             <div id="videoWrapper">
                                 {/* 소리가 나오도록 muted와 autoPlay를 제거했습니다. (브라우저 정책상 소리 있는 자동재생은 차단됨) */}
                                 <video id="promoVideo" loop playsInline controls controlsList="nodownload">
-                                    <source src="./promo_video.mp4" type="video/mp4" />
+                                    {/* 깃허브 페이지 하위 경로 호환을 위해 절대경로가 아닌 상대경로 유지 */}
+                                    <source src="promo_video.mp4" type="video/mp4" />
                                     <source src="./promo9.mp4" type="video/mp4" />
                                     <source src="https://assets.mixkit.co/videos/preview/mixkit-vertical-shot-of-a-woman-using-a-smartphone-41484-large.mp4" type="video/mp4" />
                                 </video>
